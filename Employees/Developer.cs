733483bc2b2d261519dc59834baa0037a5920204
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2020-08-05 12:32:12 -0400

    Developer Team Challenge

---
+++ Employees/Developer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperTeam.Employees
+{
+    public class Developer
+    {
+        //Constructor
+        public Developer(int id, string lastname, bool hasps)
+        {
+            ID = id;
+            LastName = lastname;
+            HasPluralSight = hasps;
+        }
+
+        //Properties
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public bool HasPluralSight { get; set; }
+        public int TeamIDIndividual { get; set; }
+    }
+}
---
+++ Employees/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperTeam.Employees
+{
+    public class Team
+    {
+        //Constructor
+        public Team(string title, int teamid)
+        {
+            TeamName = title;
+            TeamID = teamid;
+        }
+        public Team(string title, int teamid, List<Developer> developers)
+        {
+            TeamName = title;
+            TeamID = teamid;
+            Developers = developers;
+        }
+
+        //Properties
+        public string TeamName { get; set; }
+        public int TeamID { get; set; }
+        public List<Developer> Developers { get; set;}
+    }
+}
